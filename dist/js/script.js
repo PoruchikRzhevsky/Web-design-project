beb29600c50d75ae593d2a398cc3f6c9da1cb56f
Артём <sheportplay@gmail.com>
2020-11-12 13:41:23 +1000

    Pagination

--- dist/js/script.js
+++ dist/js/script.js
@@ -9488,44 +9488,66 @@ const modalCallOrderEl = document.querySelector('.js-modal_call-order');
 const callOrderOverlayEl = document.querySelector('.js-overlay_call-order');
 const closeButtonCallOrderEl = document.querySelector('.js-close-button_call-order');
 const callOrderButtonEl = document.querySelectorAll('.js-call-button');
+const callForm = document.querySelector('.js-modal__form_call');
 
 closeButtonCallOrderEl.addEventListener('click', function() {
   modalCallOrderEl.classList.toggle('close');
+  body.classList.toggle('no-scroll');
+  callForm.reset();
 });
 
 callOrderButtonEl.forEach((item) => {
-	item.addEventListener('click', function() {
+	item.addEventListener('click', function(e) {
 		modalCallOrderEl.classList.toggle('close');
+		body.classList.toggle('no-scroll');
 		closeButtonCallOrderEl.focus();
+		e.preventDefault();
 	});
 });
 
 callOrderOverlayEl.addEventListener('click', function() {
   modalCallOrderEl.classList.toggle('close');
+  body.classList.toggle('no-scroll');
+  callForm.reset();
 });
 
 window.addEventListener('keydown', function (e) {
-    if(e.keyCode === 27) modalCallOrderEl.classList.add('close');
+    if(e.keyCode === 27) {
+    	modalCallOrderEl.classList.add('close');
+    	body.classList.toggle('no-scroll');
+      callForm.reset();
+    }
 }); 
 const modalProjectOrderEl = document.querySelector('.js-modal_project-order');
 const projectOrderOverlayEl = document.querySelector('.js-overlay_project-order');
 const closeButtonProjectOrderEl = document.querySelector('.js-close-button_project-order');
 const projectOrderButtonEl = document.querySelector('.js-order-project');
+const projectForm = document.querySelector('.js-modal__form_project');
 
 closeButtonProjectOrderEl.addEventListener('click', function() {
   modalProjectOrderEl.classList.toggle('close');
+  body.classList.toggle('no-scroll');
   projectOrderButtonEl.focus();
+  projectForm.reset();
 });
 
-projectOrderButtonEl.addEventListener('click', function() {
+projectOrderButtonEl.addEventListener('click', function(e) {
   modalProjectOrderEl.classList.toggle('close');
   closeButtonProjectOrderEl.focus();
+  body.classList.toggle('no-scroll');
+  e.preventDefault();
 });
 
 projectOrderOverlayEl.addEventListener('click', function() {
   modalProjectOrderEl.classList.toggle('close');
+  body.classList.toggle('no-scroll');
+  projectForm.reset();
 });
 
 window.addEventListener('keydown', function (e) {
-    if(e.keyCode === 27) modalProjectOrderEl.classList.add('close');
+    if(e.keyCode === 27) {
+    	modalProjectOrderEl.classList.add('close');
+    	body.classList.toggle('no-scroll');
+      projectForm.reset();
+    }
 }); 
--- src/js/modal-call-order.js
+++ src/js/modal-call-order.js
@@ -2,22 +2,33 @@ const modalCallOrderEl = document.querySelector('.js-modal_call-order');
 const callOrderOverlayEl = document.querySelector('.js-overlay_call-order');
 const closeButtonCallOrderEl = document.querySelector('.js-close-button_call-order');
 const callOrderButtonEl = document.querySelectorAll('.js-call-button');
+const callForm = document.querySelector('.js-modal__form_call');
 
 closeButtonCallOrderEl.addEventListener('click', function() {
   modalCallOrderEl.classList.toggle('close');
+  body.classList.toggle('no-scroll');
+  callForm.reset();
 });
 
 callOrderButtonEl.forEach((item) => {
-	item.addEventListener('click', function() {
+	item.addEventListener('click', function(e) {
 		modalCallOrderEl.classList.toggle('close');
+		body.classList.toggle('no-scroll');
 		closeButtonCallOrderEl.focus();
+		e.preventDefault();
 	});
 });
 
 callOrderOverlayEl.addEventListener('click', function() {
   modalCallOrderEl.classList.toggle('close');
+  body.classList.toggle('no-scroll');
+  callForm.reset();
 });
 
 window.addEventListener('keydown', function (e) {
-    if(e.keyCode === 27) modalCallOrderEl.classList.add('close');
+    if(e.keyCode === 27) {
+    	modalCallOrderEl.classList.add('close');
+    	body.classList.toggle('no-scroll');
+      callForm.reset();
+    }
 }); 
